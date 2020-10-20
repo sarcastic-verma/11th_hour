1b7418c43847dfe3303484b53ca9c795d45bdb56
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-10-20 11:33:36 +0530

    - added some more views

--- src/App/app.jsx
+++ src/App/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CourseCard from '../Components/Course-Card/course-card.component.jsx';
 import './app.scss';
+import Upload from "../Components/Upload-Component/upload-component.component";
 
 function App() {
     return (
         <div className="App">
             <h1> Shit added!! </h1>
             <CourseCard title="popOS"/>
+            <Upload/>
         </div>
     );
 }
