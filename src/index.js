2a32a8b44ac106bf9ef4c291afb5efd74be149c8
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-10-26 14:49:47 +0530

    - router config in index.js  - moved assets to src

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App/app';
+import App from './app/app';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import store, {persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
-            <App/>
+            <BrowserRouter>
+                <PersistGate persistor={persistor}>
+                    <App/>
+                </PersistGate>
+            </BrowserRouter>
         </React.StrictMode>
     </Provider>,
     document.getElementById('root')
