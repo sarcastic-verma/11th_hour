de1ab4cfd089527a14f44d7a913eff1380cb3dc6
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-11-14 01:34:43 +0530

    - upload section done

--- src/components/upload-section/upload-section.component.jsx
+++ src/components/upload-section/upload-section.component.jsx
@@ -1,143 +1,35 @@
-import {onFileChange, onUploadSubmission} from "../../controllers/course-controller";
+import {onFileChange} from "../../controllers/course-controller";
 import React, {Component} from "react";
 
 
 export default class UploadSection extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            lectures: [],
-            courseThumbnail: [],
-            resources: [],
-            resourcesProgress: 0,
-            lecturesProgress: 0,
-            courseThumbnailProgress: 0,
-            url: null
-        };
-    }
-
-    setFiles = (newFile) => {
-        const type = this.props.title;
-        type === "lectures" ?
-            this.setState((prevState) => (
-                {
-                    lectures: [...prevState.lectures, newFile]
-                }
-            ))
-            : type === "resources" ?
-            this.setState((prevState) => (
-                {
-                    resources: [...prevState.resources, newFile]
-                }
-            ))
-            : this.setState(() => (
-                {
-                    courseThumbnail: [newFile]
-                }
-            ));
-    };
-
-    setProgress = (progress) => {
-        const type = this.props.title;
-        type === "lectures" ?
-            this.setState(() => (
-                {
-                    lectureProgress: progress
-                }
-            ))
-            : type === "resources" ?
-            this.setState(() => (
-                {
-                    resourcesProgress: progress
-                }
-            ))
-            : this.setState(() => (
-                {
-                    courseThumbnailProgress: progress,
-                }
-            ));
-    };
-
-    setUrl = (url) => {
-        // const type = this.props.title;
-        this.setState(() => ({url}));
-    };
-
-    onComplete = () => {
-        const type = this.props.title;
-        type === "lectures" ?
-            this.setState(() => (
-                {
-                    lectures: [],
-                    lectureProgress: 0
-                }
-            ))
-            : type === "resources" ?
-            this.setState(() => (
-                {
-                    resources: [],
-                    resourcesProgress: 0
-                }
-            ))
-            : this.setState(() => (
-                {
-                    courseThumbnail: null,
-                    thumbnailProgress: 0
-                }
-            ));
     }
 
     render() {
-        const {title} = this.props;
+        const {title, progress, displayables, setFiles} = this.props;
         return <div>
             <h3>Upload {title}</h3>
             <div className="row">
-                <progress value={title === "lectures" ?
-                    this.state.lecturesProgress
-                    : title === "resources" ?
-                        this.state.resourcesProgress
-                        : this.state.courseThumbnailProgress} max="100" className="progress"/>
+                <progress value={progress} max="100" className="progress"/>
             </div>
-            <h3>{title === "lectures" ?
-                this.state.lecturesProgress
-                : title === "resources" ?
-                    this.state.resourcesProgress
-                    : this.state.courseThumbnailProgress} %</h3>
+            <h3>{progress} %</h3>
             <br/>
             <div className="file-field input-field">
                 <div className="btn">
                     {title !== "course thumbnail" ?
-                        <input type="file" multiple onChange={e => onFileChange(e, this.setFiles)}/> :
-                        <input type="file" onChange={e => onFileChange(e, this.setFiles)}/>}
+                        <input type="file" multiple onChange={e => onFileChange(e, title, setFiles)}/> :
+                        <input type="file" onChange={e => onFileChange(e, title, setFiles)}/>}
                 </div>
             </div>
             <br/>
             <br/>
-            {title === "lectures" ?
-                this.state.lectures.map((lecture) => {
-                    return <h1 key={lecture.id}>{`${lecture.name} `}</h1>
-                })
-                : title === "resources" ?
-                    this.state.resources.map((resource) => {
-                        return <h1 key={resource.id}>{`${resource.name} `}</h1>
-                    })
-                    :this.state.courseThumbnail.map((courseThumbnail) => {
-                        return <h1 key={courseThumbnail.id}>{`${courseThumbnail.name} `}</h1>
-                    })
+            {displayables ?
+                displayables.map((displayable) => {
+                    return <h1 key={displayable.id}>{`${displayable.name} `}</h1>
+                }) : <div/>
             }
-            <button
-                onClick={async (e) => {
-                    let files;
-                    title === "lectures" ?
-                        files = this.state.lectures
-                        : title === "resources" ?
-                        files = this.state.resources
-                        : files = this.state.courseThumbnail;
-                    await onUploadSubmission(e, files, title, this.setProgress, this.setUrl, this.onComplete);
-                }} className="waves-effect waves-light btn">
-                Upload
-            </button>
         </div>
-     };
+    };
 }
