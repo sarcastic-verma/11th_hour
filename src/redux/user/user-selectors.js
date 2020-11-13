7ed78df24e3296263362df984706f54156e7436f
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-11-14 01:24:11 +0530

    - created some selectors

--- src/redux/user/user-selectors.js
+++ src/redux/user/user-selectors.js
@@ -3,6 +3,21 @@ import {createSelector} from 'reselect';
 const selectUser = state => state.user;
 
 export const selectCurrentUser = createSelector(
-  [selectUser],
-  user => user.currentUser
+    [selectUser],
+    user => user.currentUser
+);
+
+export const selectInstructorName = createSelector(
+    [selectUser],
+    user => user.name,
+);
+
+export const selectInstructorId = createSelector(
+    [selectUser],
+    user => user.userId,
+);
+
+export const selectCollegeId = createSelector(
+    [selectUser],
+    user => user.collegeId,
 );
